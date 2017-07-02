8f2a5a8442f97aeea2a4cd7aea82e16724226d10
sididu <biang.poker888@gmail.com>
2017-07-02 22:38:34 +0700

    Creating Views Page

---
+++ resources/views/rp3mum/rp3mum_create.blade.php
@@ -0,0 +1,38 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <div><h3><a href="home"><img class="push-left" src="dist/img/logo-kejaksaan.png" width="100"></a>
+  Dashboard
+  <small>Control panel</small>
+</h3>
+</div>
+@stop
+
+@section('content')
+<div class="box box-success">
+  <div class="box-header with-border">
+  <h3 class="box-title text-uppercase">DAFTAR KASUS BARU <small>[RP1]</small></h3>
+    <div class="pull-right">
+      <a href="frp1" class="btn btn-defaul btn-xs"> Tambah Kasus &nbsp; <span class="glyphicon glyphicon-plus"></span></a>
+    </div>
+  </div>
+  <div class="box-body">
+
+
+
+
+    <p class="text-center"><img src="dist/img/UnderConstruct.png" alt=""></p>
+    
+
+
+
+
+
+
+  </div>
+  <div class="panel-footer">Panel footer</div>
+</div>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('jabatan', 'JabatanController@index');
+
+Route::get('Obyek', 'ObyekController@index');
+
+Route::get('Pasal', 'PasalController@index');
+
+Route::get('Rp1', 'Rp1Controller@index');
+
+Route::get('Rp2', 'Rp2Controller@index');
+
+Route::get('Rp3mum', 'Rp3mumController@index');
+
+Route::get('Rp3sus', 'Rp3susController@index');
+
+Route::get('Subyek', 'SubyekController@index');
+
+Route::get('User', 'UserController@index');
+
+
