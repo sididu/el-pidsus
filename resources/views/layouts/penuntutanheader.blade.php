6d19c0c2952d6f5b16aa0ad5f56e18b84f85c10c
sididu <biang.poker888@gmail.com>
2017-07-04 07:38:41 +0700

    update content header

--- resources/views/layouts/penuntutanheader.blade.php
+++ resources/views/layouts/penuntutanheader.blade.php
@@ -1,7 +1,7 @@
 <h3>
 <a href="home"><img class="push-left" src="dist/img/logo-kejaksaan.png" width="100" class="text-titlecase"></a>
-          @yield('maintitle')
-          <small> &nbsp; @yield('mainsubtitle')</small>
+         {{--  @yield('maintitle')
+          <small> &nbsp; @yield('mainsubtitle')</small> --}}
 <div class="pull-right">
             COMING SOON
             <a href="obyek" class="btn btn-app"><i class="fa fa-cubes"></i> RB-1</a>  
--- resources/views/layouts/penyidikanheader.blade.php
+++ resources/views/layouts/penyidikanheader.blade.php
@@ -1,7 +1,7 @@
 <h3>
 <a href="home"><img class="push-left" src="dist/img/logo-kejaksaan.png" width="100" class="text-titlecase"></a>
-          @yield('maintitle')
-          <small> &nbsp; @yield('mainsubtitle')</small>
+         {{--  @yield('maintitle')
+          <small> &nbsp; @yield('mainsubtitle')</small> --}}
 <div class="pull-right">
             <a href="rp1" class="btn btn-app"><i class="fa fa-list-ol"></i> RP-1</a>                    
             <a href="rp2" class="btn btn-app"><i class="fa fa-list-ol"></i> RP-2</a>                    
